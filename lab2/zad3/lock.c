d720881745eef3052271e27586285958a8e26a9c
grzegorzborkowski <gb.grzegorzborkowski@gmail.com>
2016-03-17 17:17:42 +0100

    Refactor seting locks

--- lab2/zad3/lock.c
+++ lab2/zad3/lock.c
@@ -1,15 +1,17 @@
 #include "lock.h"
 
-void set_write_lock(int open_descriptor, off_t offset) {
+int execute_lock(int open_descriptor, int function, int type, int offset) {
     struct flock flock1;
-    int fcntl_result;
-    flock1.l_type = F_WRLCK; /* type of lock */
-    flock1.l_whence = SEEK_SET; /* from the beginiing of the file */
+    flock1.l_type = type; /* type of lock */
+    flock1.l_whence = SEEK_SET;  /* from the beginiing of the file */
     flock1.l_start = offset; /* distance from SEEK_SET */
     flock1.l_len = 1; /* locking just one byte */
+    return fcntl(open_descriptor, function, &flock1);
+}
 
-    /* descriptor, commend, arguments */
-    fcntl_result = fcntl(open_descriptor, F_SETLK, &flock1);
+void set_write_lock(int open_descriptor, off_t offset) {
+    int fcntl_result;
+    fcntl_result = execute_lock(open_descriptor, F_SETLK, F_WRLCK, offset);
     if(fcntl_result == -1) {
         printf("Setting write lock error\n");
         exit(EXIT_FAILURE);
@@ -19,14 +21,8 @@ void set_write_lock(int open_descriptor, off_t offset) {
 }
 
 void set_read_lock(int open_descriptor, off_t offset) {
-    struct flock flock1;
     int fcntl_result;
-    flock1.l_type = F_RDLCK;
-    flock1.l_whence = SEEK_SET;
-    flock1.l_start = offset;
-    flock1.l_len = 1;
-
-    fcntl_result = fcntl_result(open_descriptor, F_SETLK, &flock1);
+    fcntl_result = execute_lock(open_descriptor, F_SETLK, F_RDLCK, offset);
     if(fcntl_result == -1) {
         printf("Setting read lock error\n");
         exit(EXIT_FAILURE);
@@ -35,7 +31,28 @@ void set_read_lock(int open_descriptor, off_t offset) {
     }
 }
 
-void list_locks(int open_descriptor);
+/*void list_locks(int open_descriptor) {
+    int fcntl_result;
+
+    fcntl_result = fcntl(open_descriptor, F_GETLK, &flock1);
+    if(fcntl_result == -1) {
+        printf("Error in fcntl F_GETLK (GET LOCK)\n");
+        exit(EXIT_FAILURE);
+    }
+
+} */
+
+
+void unlock(int open_descriptor, off_t offset) {
+    int fcntl_result;
+    fcntl_result = execute_lock(open_descriptor, F_SETLK, F_UNLCK, offset);
+    if(fcntl_result == -1) {
+        printf("Unlocking failed\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Removing lock succeed\n");
+    }
+}
 
 void interactive_mode(int open_descriptor) {
     char option;
@@ -52,14 +69,16 @@ void interactive_mode(int open_descriptor) {
                 break;
             case 'r':
                 printf("Please enter byte number\n");
-                scanf("%d, &offset");
+                scanf("%d", &offset);
                 set_read_lock(open_descriptor, offset);
                 break;
             case 'l':
                 /* list_locks(); */
                 break;
             case 'u':
-                /* unlock(); */
+                printf("Enter the byte number\n");
+                scanf("%d", &offset);
+                unlock(open_descriptor, offset);
                 break;
             case 'd':
                 /* display_char(); */
