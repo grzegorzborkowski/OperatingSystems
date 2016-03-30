f5ab8188bfb7589e9afe0d75c16ae606b92c9424
grzegorzborkowski <gb.grzegorzborkowski@gmail.com>
2016-03-30 14:23:57 +0200

    [lab3] Add traversing through directory.

--- lab3/zad2/fcounter.c
+++ lab3/zad2/fcounter.c
@@ -1,13 +1,40 @@
 #include "fcounter.h"
 
-void run(char* dir, int wait_flag, int display_flag) {
+void run(char* dir_path, char *ext, int wait_flag, int display_flag) {
+	int file_count;
+	DIR *dir;
+	dirent_t *dirent_file;
+
+	file_count = 0;
+
+	dir = opendir(dir_path);
+	if(dir == NULL) {
+		printf("Error while opening the directory\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while(dirent_file = readdir(dir)) {
+		if(!strcmp(dirent_file->d_name, "." )|| !strcmp(dirent_file->d_name, "..")) continue;
+
+		if(dirent_file->d_type == DT_DIR) {
+			printf("Directory %s\n", dirent_file->d_name);
+		}
+		else if(dirent_file->d_type == DT_REG) {
+			printf("Regular file %s\n", dirent_file->d_name);
+			if(ext==NULL) {
+				file_count += 1;
+			}
+		}
+	}
+	closedir(dir);
 }
 
 int main(int argc, char **argv) {
-    char *dir;
+    char *dir, *ext;
     int opt;
     int wait_flag = 0;
     int display_flag = 0;
+    int path_browse_flag = 0;
 
 
     dir = NULL;
@@ -16,19 +43,26 @@ int main(int argc, char **argv) {
         dir = argv[1];
     }
 
-    while ((opt = getopt(argc, argv, "wv")) == 0) {
+    ext = getenv("EXT_TO_BROWSE");
+
+    while ((opt = getopt(argc, argv, "wvp")) == 0) {
         switch(opt) {
+        	case 'p': path_browse_flag=1; break;
             case 'w': wait_flag=1; break;
             case 'v': display_flag=1; break;
         }
     }
 
-    /* default option */
-    if(dir == NULL || strlen(dir)) {
+    /* default option -> without flag p */
+    if(path_browse_flag ==0 && (dir == NULL || strlen(dir))) {
         dir = ".";
     }
 
-    run(dir, wait_flag, display_flag);
+    if(path_browse_flag == 1) {
+    	dir = getenv("PATH_TO_BROWSE");
+    }
+
+    run(dir, ext, wait_flag, display_flag);
 
     return 0;
 }
--- lab3/zad2/fcounter.h
+++ lab3/zad2/fcounter.h
@@ -2,9 +2,14 @@
 #define _fcounter_H
 
 #include <stdio.h>
+#include <dirent.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdlib.h>
 
+typedef struct dirent dirent_t;
 
 #endif
 
